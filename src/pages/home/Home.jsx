2964b5e3fd1037666fe6929996f8aa9ded7cedd8
Asadbek <asadbekmirmahmudov3@gmail.com>
2024-07-31 23:31:44 -0700

    add sorted

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,13 @@
 import React from "react";
 import "./Home.css";
 
-function Home({toast}) {
-  const salom =()=>{
-    toast.info("sasajjsajjkjkfjhfjhfdhjfhjdhjdf")
-    toast.success("sasajjsajjkjkfjhfjhfdhjfhjdhjdf")
-    toast.warn("sasajjsajjkjkfjhfjhfdhjfhjdhjdf")
-    toast.error("sasajjsajjkjkfjhfjhfdhjfhjdhjdf")
-    toast.info("sasajjsajjkjkfjhfjhfdhjfhjdhjdf")
-    toast.info("sasajjsajjkjkfjhfjhfdhjfhjdhjdf")
-  }
+function Home({ toast }) {
   return (
     <>
       <div className="home">
         <div className="homeContainer">
           <div className="widgets">
-            {/* <AppointWidget /> */}
-
-            <div className="breaker">
-              <h1 onClick={salom} >home</h1>
-            </div>
-            {/* <TasksWidget /> */}
+            <div className="breaker"></div>
           </div>
         </div>
       </div>
